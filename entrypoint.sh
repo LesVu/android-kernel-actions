eb4cc59e29ecd9308ed6b794d8641aa6c7ab1b54
LesVu <66196443+LesVu@users.noreply.github.com>
2024-03-30 21:42:06 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -209,8 +209,8 @@ start_time="$(date +%s)"s
 tag="$(git branch | sed 's/*\ //g')"
 echo "branch/tag: $tag"
 echo "make options:" "$arch_opts" "$make_opts" "$host_make_opts"
-msg "::group::Generating defconfig from \`make $defconfig\`..."
-if ! make O=out "$arch_opts" "$make_opts" "$host_make_opts" $defconfig; then
+msg "::group::Generating defconfig from make $defconfig..."
+if ! make O=out "$arch_opts" "$make_opts" "$host_make_opts" "$defconfig"; then
     err "Failed generating .config, make sure it is actually available in arch/${arch}/configs/ and is a valid defconfig file"
     exit 2
 fi
